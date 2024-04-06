f56fc3be383d5b89b9408275975a36b86513a8cc
fujimakiaya <163990085+fujimakiaya@users.noreply.github.com>
2024-04-06 16:13:32 +0900

    Update employee_agree.js

--- employee_agree.js
+++ employee_agree.js
@@ -1,37 +1,48 @@
-var str1 = sessionStorage.getItem('str1');
-var str2 = sessionStorage.getItem('str2');
+var count = 0;
 
-var array2 = str1.split(',');
-var array3 = str2.split(',');
-
-function kanma(fig) {
-    if(array3[fig]){
-        if(array3[fig] > 0){
-            array3[fig] = Number(array3[fig]).toLocaleString();
+function getValue() {
+    var str1 = sessionStorage.getItem('str1');
+    var str2 = sessionStorage.getItem('str2');
+    var array2 = str1.split(',');
+    var array3 = str2.split(',');
+    
+    function kanma(fig) {
+        if(array3[fig]){
+            if(array3[fig] > 0){
+                array3[fig] = Number(array3[fig]).toLocaleString();
+            }
         }
+    }
+    
+    kanma(26);
+    kanma(28);
+    kanma(31);
+    kanma(34);
+    kanma(37);
+    kanma(40);
+    kanma(43);
+    kanma(46);
+    kanma(49);
+    kanma(52);
+    kanma(55);
+    kanma(58);
+    kanma(61);
+    
+    var htmlString = document.body.innerHTML;
+    for(var i = 0; i < array2.length; i++){
+        htmlString = htmlString.replace(array2[i], array3[i]);
+    };
+    
+    document.body.innerHTML = htmlString;
+
+    count++;
+    if(count == 5){
+        clearInterval(intervalID);
     }
 }
 
-kanma(26);
-kanma(28);
-kanma(31);
-kanma(34);
-kanma(37);
-kanma(40);
-kanma(43);
-kanma(46);
-kanma(49);
-kanma(52);
-kanma(55);
-kanma(58);
-kanma(61);
+var intervalID = setInterval(getValue(), 300);
 
-var htmlString = document.body.innerHTML;
-for(var i = 0; i < array2.length; i++){
-    htmlString = htmlString.replace(array2[i], array3[i]);
-};
-
-document.body.innerHTML = htmlString;
 
 createPrintButton('print-button', '印刷はコチラ');
 createCloseButton('print-close', 'ウインドウを閉じる');
