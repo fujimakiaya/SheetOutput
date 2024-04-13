e2df610a85c3733ca31af49b063ccbe3215d5561
fujimakiaya <163990085+fujimakiaya@users.noreply.github.com>
2024-04-13 16:19:21 +0900

    Add files via upload

---
+++ KV_shikaku_employee_find_v2.7.js
@@ -0,0 +1,46 @@
+const LOCATED_URL = new URL(window.location.href);
+const PARAMS = LOCATED_URL.searchParams; //渡したパラメータを取得。今回は社員番号を取得。
+
+let EMPLOYEE_FIND_COUNTER = 1; //非同期処理の回数カウンター
+const MATCH_EMPLOYEE = setInterval(match_employee, 100); //非同期処理の設定と削除用変数
+
+async function match_employee() {
+    /* 社員番号と該当するレコードを探索。非同期処理。 */
+    try {
+        const employeeData = document.getElementsByTagName("tbody")[0].rows; //全レコードを取得
+        if (employeeData[0] === undefined) {
+            throw new Error('loading error');
+        }
+        clearInterval(MATCH_EMPLOYEE); //非同期処理を削除
+        /* 社員番号と該当したレコードのリンクに遷移 */
+        for (let i = 0; i < employeeData.length; i++) {
+            /* 社員番号が登録されていない場合、姓と名で探索 */
+            if (PARAMS.get("employeeId") !== "") {
+                if (employeeData[i].getElementsByTagName("td")[1].getElementsByTagName("div")[0].textContent === PARAMS.get("employeeId")) {
+                    const employeeLink =  employeeData[i].getElementsByTagName("td")[0].getElementsByTagName("a")[0].getElementsByTagName("i")[0];
+                    employeeLink.click();
+                }
+            } else {
+                if (employeeData[i].getElementsByTagName("td")[2].getElementsByTagName("div")[0].textContent === PARAMS.get("姓") && employeeData[i].getElementsByTagName("td")[3].getElementsByTagName("div")[0].textContent === PARAMS.get("名") && employeeData[i].getElementsByTagName("td")[1].getElementsByTagName("div")[0].textContent === "") {
+                    const employeeLink =  employeeData[i].getElementsByTagName("td")[0].getElementsByTagName("a")[0].getElementsByTagName("i")[0];
+                    employeeLink.click();
+
+                }
+            }
+        }
+        return;
+    } catch(e) {
+        console.error("employee find error: ", e.message);
+    }
+    console.log("employee find try: ", EMPLOYEE_FIND_COUNTER++);
+    /* カウンターが一定回数を超えたら非同期処理を停止 */
+    if (EMPLOYEE_FIND_COUNTER > 30) {
+        clearInterval(MATCH_EMPLOYEE);
+        console.log("error: employees page could not be loaded.");
+
+        alert("該当するデータがありません");   /* 2024/01/24 */
+        window.close();                       /* 2024/01/24 */
+    }
+}
+
+    //document.getElementsByTagName("main")[0].style.display = "none";
